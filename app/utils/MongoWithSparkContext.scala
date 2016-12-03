fbdf765db733731260dfc47eddf7f123dca9ab3d
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2016-12-03 19:46:32 +0530

    sprak context with mongodb trait

---
+++ app/utils/MongoWithSparkContext.scala
@@ -0,0 +1,27 @@
+package utils
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+import com.mongodb.spark.MongoConnector
+import com.mongodb.spark.config.WriteConfig
+
+/**
+  * Created by jyothi on 3/12/16.
+  */
+private[utils] trait MongoWithSparkContext {
+
+  def getSparkContext(args: Array[String]): SparkContext = {
+    val uri: String = args.headOption.getOrElse("mongodb://localhost/jyothi.users") //DB: jyothi, collection: users
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("SparkMongoScalaPlay")
+      .set("spark.app.id", "SparkMongoScalaPlay")
+      .set("spark.mongodb.input.uri", uri)
+      .set("spark.mongodb.output.uri", uri)
+
+    val sc = new SparkContext(conf)
+    MongoConnector(sc).withDatabaseDo(WriteConfig(sc), {db => db.drop()})
+    sc
+  }
+
+}
