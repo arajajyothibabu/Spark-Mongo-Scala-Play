e9d4929d70ac0c49d8149a700446c95f78d5851d
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-12-05 16:45:12 +0530

    basic operations with mongospark

--- app/utils/MongoUtility.scala
+++ app/utils/MongoUtility.scala
@@ -16,39 +16,37 @@ object MongoUtility extends MongoWithSparkContext {
     val sc = getSparkContext(args) // Don't copy and paste as its already configured in the shell
 
     import com.mongodb.spark._
-
-    // Saving data from an RDD to MongoDB
     import org.bson.Document
-    val documents = sc.parallelize((1 to 10).map(i => Document.parse(s"{test: $i}")))
-    MongoSpark.save(documents)
+
+    //Reading data from MongoDb as RDD
+    val companiesRdd = MongoSpark.load(sc) //we can also read with custom readConfig
+    println("Total Companies:: " + companiesRdd.count())
+    println("One Company:: " + companiesRdd.first.toJson)
+
+    import com.mongodb.spark.config._
+
+    // Filtering an rdd in Spark
+    val filteredRdd = companiesRdd.filter(doc => doc.getInteger("number_of_employees") == 500) //companies with more than 500
+    println("Filtered Companies with more than 500 employees:: " + filteredRdd.count)
+    println("Filtered One Company:: " + filteredRdd.first.toJson)
+
+    // Filtering an rdd using an aggregation pipeline before passing to Spark
+    val aggregatedRdd = companiesRdd.withPipeline(Seq(Document.parse("{ $match: { competitions: { $eq : [] } } }")))
+    println("Aggregation Companies with no competitors:: " + aggregatedRdd.count)
+    println("Aggregation One Company:: " + aggregatedRdd.first.toJson)
 
     // Saving data with a custom WriteConfig
-    import com.mongodb.spark.config._
     val writeConfig = WriteConfig(Map("collection" -> "spark", "writeConcern.w" -> "majority"), Some(WriteConfig(sc)))
 
     val sparkDocuments = sc.parallelize((1 to 10).map(i => Document.parse(s"{spark: $i}")))
     MongoSpark.save(sparkDocuments, writeConfig)
 
-    // Loading and analyzing data from MongoDB
-    val rdd = MongoSpark.load(sc)
-    println(rdd.count)
-    println(rdd.first.toJson)
-
-    // Loading data with a custom ReadConfig
+    // Loading and analyzing data from MongoDB with a custom ReadConfig
     val readConfig = ReadConfig(Map("collection" -> "spark", "readPreference.name" -> "secondaryPreferred"), Some(ReadConfig(sc)))
     val customRdd = MongoSpark.load(sc, readConfig)
-    println(customRdd.count)
-    println(customRdd.first.toJson)
+    println("Custom Count:: " + customRdd.count)
+    println("Custom One Document:: " + customRdd.first.toJson)
 
-    // Filtering an rdd in Spark
-    val filteredRdd = rdd.filter(doc => doc.getInteger("test") > 5)
-    println(filteredRdd.count)
-    println(filteredRdd.first.toJson)
-
-    // Filtering an rdd using an aggregation pipeline before passing to Spark
-    val aggregatedRdd = rdd.withPipeline(Seq(Document.parse("{ $match: { test : { $gt : 5 } } }")))
-    println(aggregatedRdd.count)
-    println(aggregatedRdd.first.toJson)
   }
 
 }
