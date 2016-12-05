f0df2383417eec40a4991e20871b2eeadfd44a94
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-12-05 20:21:07 +0530

    spark streams

--- app/utils/SparkStreams.scala
+++ app/utils/SparkStreams.scala
@@ -1,7 +1,7 @@
 package utils
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.SQLContext
 
 /**
   * The spark streams code example adapted from: http://spark.apache.org/docs/latest/streaming-programming-guide.html
@@ -11,33 +11,34 @@ import org.apache.spark.sql.{SQLContext, SparkSession}
 object SparkStreams extends MongoWithSparkContext {
 
   /**
-    * Run this main method to see the output of this quick example or copy the code into the spark shell
-    *
     * See: http://spark.apache.org/docs/latest/streaming-programming-guide.html
-    * Requires: Netcat running on localhost:9999
+    * Requires: Netcat running on localhost:9999 using command `nc -lk 9999`
     *
     * @param args takes an optional single argument for the connection string
     * @throws Throwable if an operation fails
     */
   def main(args: Array[String]): Unit = {
-    val sc = getSparkContext(args) // Don't copy and paste as its already configured in the shell
+    val sc = getSparkContext(args)
 
     import com.mongodb.spark.sql._
     import org.apache.spark.streaming._
 
     val ssc = new StreamingContext(sc, Seconds(1))
 
-    val lines = ssc.socketTextStream("localhost", 9999)
+    val lines = ssc.socketTextStream("localhost", 9999) //keep writing in the terminal
 
     val words = lines.flatMap(_.split(" "))
     val pairs = words.map(word => (word, 1))
     val wordCounts = pairs.reduceByKey(_ + _)
     wordCounts.foreachRDD({ rdd =>
-      val sparkSession = SparkSessionSingleton.getInstance(rdd.sparkContext)
-      import sparkSession.implicits._
+      val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+      import sqlContext.implicits._
 
       val wordCounts = rdd.map({ case (word: String, count: Int) => WordCount(word, count) }).toDF()
-      wordCounts.write.mode("append").mongo()
+      wordCounts.show()
+      import com.mongodb.spark.config.WriteConfig
+      val writeConfig  = WriteConfig(Map("collection" -> "streams", "writeConcern.w" -> "majority"), Some(WriteConfig(sc)))
+      wordCounts.write.mode("append").mongo(writeConfig)
     })
 
     ssc.start() // Start the computation
@@ -48,13 +49,13 @@ object SparkStreams extends MongoWithSparkContext {
   case class WordCount(word: String, count: Int)
 
   /** Lazily instantiated singleton instance of SQLContext */
-  object SparkSessionSingleton {
+  object SQLContextSingleton {
 
-    @transient private var instance: SparkSession = _
+    @transient private var instance: SQLContext = _
 
-    def getInstance(sparkContext: SparkContext): SparkSession = {
+    def getInstance(sparkContext: SparkContext): SQLContext = {
       if (Option(instance).isEmpty) {
-        instance = SparkSession.builder().getOrCreate()
+        instance = SQLContext.getOrCreate(sparkContext)
       }
       instance
     }
